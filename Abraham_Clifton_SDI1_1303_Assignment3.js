b77698cf35c6c9cae02d951a3bb54758f8be8637
Lastangel <kingpin2086@fullsail.edu>
2013-03-21 05:42:44 -0300

    Create Abraham_Clifton_SDI1_1303_Assignment3.js

---
+++ Abraham_Clifton_SDI1_1303_Assignment3.js
@@ -0,0 +1,27 @@
+//Clifton Abraham
+// 03/21/13
+// SDI1, 1303
+// PROJECT 3
+
+/* This project i decided to blend both parts of my story,my goal is
+ to get to the point with straight forward code and to ultimately
+ make this a rough draft for my app game idea.*/
+
+// Global varibles
+var Warrior;
+var goblinBoss;
+var goons;
+var hunting =
+        "Lastangel Leaves his home" // string 1 
+        + "to find food for his family.";
+var LAhealth = 100;
+var GBhealth = 120;
+var gnsHealth = 40; // goons health
+var Weapons =  //Array 1
+    ["ShotGun",
+     "pistol",
+     "AutoMatic"];
+var Paths = // Array 2
+    ["Forward",
+    "Left",
+     "Right"];
